b77c9f8745d7144dac4b343d231cb3088f9a2820
Aaditya Jain <155458768+AadityaJain0128@users.noreply.github.com>
2025-05-29 23:26:39 +0530

    Updated pokedex.py

--- app/pokedex.py
+++ app/pokedex.py
@@ -1,72 +1,90 @@
-import ollama
 import json
 import requests
 from uuid import uuid4
 import os
 import time
+import base64
+from dotenv import load_dotenv
 
 
-PROMPT = '''
-            You have to respond like you are a Pokedex.
-            Given an image of a Pokémon, provide the following information in JSON format, if any object in the image is not identified, you can return 'Not Applicable' and all values should be in string format:
-            {
-                'name': 'Pokémon name',
-                'description': 'Brief description of the Pokémon' (about 15-25 words),
-                'health': 'Health points',
-                'height': 'Height in meters' (it should always include the unit i.e. m and dont specify anything else),
-                'weight': 'Weight in kilograms' (it should always include the unit i.e. kg and dont specify anything else),
-                'type': ['Primary type', 'Secondary type (if applicable)']
-            }
-            Please respond with the above JSON structure only with proper delimeter (,), it should strictly have quotes as strings, no extra content and no text formatting.
-
-            You have to respond as a Pokedex. Given an image of a Pokémon, identify the Pokémon and provide the following information in JSON format. If any object in the image is not identified, return 'Not Applicable'. Ensure all values are in string format and follow the exact JSON structure below. Do not include any extra content or text formatting.
-        '''
+load_dotenv()
+API_URL = os.environ.get("API_URL")
+API_KEY = os.environ.get("API_KEY")
 
 PROMPT = '''
             You have to respond as a Pokedex. Given an image of a Pokémon, identify the Pokémon and provide the following information in JSON format.
             
-            Ensure to use the image to determine the Pokémon's details accurately. If you cannot identify the Pokémon, return 'Not Applicable' in the respective fields.
+            Ensure to use the image to determine the Pokémon's details accurately.
             If you know the name of the Pokemon, you should give appropriate information about it in all fields.
+            
             {
-                'name': 'Pokémon name',
-                'description': 'Brief description of the Pokémon (about 15-25 words)',
-                'health': 'Health points',
-                'height': 'Height in meters (include the unit, e.g., 1.5 m)',
-                'weight': 'Weight in kilograms (include the unit, e.g., 50 kg)',
-                'type': ['Primary type', 'Secondary type (if applicable)']
-            }
-
-            For example, if the Pokémon is Pikachu:
-            {
-                'name': 'Pikachu',
-                'description': 'Electric-type Pokémon known for its yellow fur and ability to generate electricity.',
-                'health': '35',
-                'height': '0.4 m',
-                'weight': '6 kg',
-                'type': ['Electric']
+                "name": "Pokémon name",
+                "description": "Brief description of the Pokémon (about 20-30 words)",
+                "health": "Health points",
+                "height": "Height in meters (include the unit, e.g., 1.5 m)",
+                "weight": "Weight in kilograms (include the unit, e.g., 50 kg)",
+                "type": ["Primary type", "Secondary type (if applicable)"]
             }
             
-            If any object in the image is not identified, return 'Not Applicable'. Ensure all values are in string format and follow the exact JSON structure below. Do not include any extra content or text formatting.
+            If any object in the image is not identified, return only "NA" in all fields. For example :-
+            {
+                "name": "NA",
+                "description": "NA",
+                "health": "NA",
+                "height": "NA",
+                "weight": "NA",
+                "type": ["NA"]
+            }
+            Ensure all values are in string format and follow the JSON structure described above. Do not include any extra content or text formatting.
+            You should keep in mind that you don't have to describe the pokemon according to what their significance was in the show. You should only describe their attributes.
         '''
 
 
 def fetch_response(img_path, folder):
-    res = ollama.generate(model="llava:13b", prompt=PROMPT, images=[img_path])["response"]
-    l = res.find("{")
-    r = res.rfind("}") + 1
-    res = res[l : r]
-    print(res)
-    res = json.loads(res)
+    with open(img_path, "rb") as f:
+        image_data = base64.b64encode(f.read()).decode("utf-8")
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messages": [
+            {
+                "role": "system",
+                "content": PROMPT
+            },
+            {
+                "role": "user",
+                "content": [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image_data}"}}]
+            }
+        ],
+        "model": "meta-llama/llama-4-maverick-17b-128e-instruct",
+        "temperature": 1,
+        "max_completion_tokens": 1024,
+        "top_p": 1,
+        "stream": False,
+        "response_format": {
+            "type": "json_object"
+        },
+        "stop": None
+    }
+
+    res = requests.post(API_URL, headers=headers, json=payload)
+    res = res.json()["choices"][0]["message"]["content"]
     
+    l, r = res.find("{"), res.rfind("}") + 1
+    res = json.loads(res[l:r])
 
     audio_path = "voices/no-content.wav"
-    if res["name"] != "Not Applicable":
+    if res["name"].lower() != "na":
         generate_audio = requests.post("https://api.fakeyou.com/tts/inference", json={"tts_model_token": "weight_dh8zry5bgkfm0z6nv3anqa9y5", "uuid_idempotency_token": str(uuid4()), "inference_text": res["name"] + ",\n" + res["description"] + ".\n", "wait": True}).json()
 
         flag = True
         status = "started"
         while status != "complete_success":
-            time.sleep(3)
+            time.sleep(2)
             fetch_audio = requests.get(f"https://api.fakeyou.com/tts/job/{generate_audio['inference_job_token']}").json()
             status = fetch_audio["state"]["status"]
             if status in ["complete_failure", "dead"]:
@@ -77,7 +95,7 @@ def fetch_response(img_path, folder):
         if flag:
             file_path = fetch_audio["state"]["maybe_public_bucket_wav_audio_path"]
             print(file_path)
-            audio = requests.get("https://storage.googleapis.com/vocodes-public" + file_path)
+            audio = requests.get("https://cdn-2.fakeyou.com" + file_path)
 
             audio_path = "voices/" + file_path.split("/")[-1]
             with open(os.path.join(folder, audio_path), "wb") as audio_file:
